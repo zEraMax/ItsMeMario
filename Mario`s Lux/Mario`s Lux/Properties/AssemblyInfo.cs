66565a726ebe8ef827e8d80745cfa10d2c872b9d
Mario Gabriell <mariogk01@gmail.com>
2016-03-31 21:13:25 -0300

    Version update

--- Mario`s Lux/Mario`s Lux/Properties/AssemblyInfo.cs
+++ Mario`s Lux/Mario`s Lux/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("ace38cee-7b1f-47ce-aa83-a71fe2949919")]
 
-[assembly: AssemblyVersion("1.3.3.7")]
-[assembly: AssemblyFileVersion("1.3.3.7")]
+[assembly: AssemblyVersion("1")]
+[assembly: AssemblyFileVersion("1")]
