ff427b6cf9e3a960cb0dfc402534d9e44f819023
Mario Gabriell <mariogk01@gmail.com>
2016-04-05 17:08:16 -0300

    New lux

---
+++ Mario`s Lux/Mario`s Template/Modes/Active.cs
@@ -0,0 +1,15 @@
+﻿namespace Mario_s_Template.Modes
+{
+    /// <summary>
+    /// This mode will always run
+    /// </summary>
+    internal class Active
+    {
+        /// <summary>
+        /// Put in here what you want to do when the mode is running
+        /// </summary>
+        public static void Execute()
+        {
+        }
+    }
+}
---
+++ Mario`s Lux/Mario`s Template/Modes/LastHit.cs
@@ -0,0 +1,23 @@
+﻿using Mario_s_Lib;
+using static Mario_s_Template.Menus;
+using static Mario_s_Template.SpellsManager;
+
+namespace Mario_s_Template.Modes
+{
+    /// <summary>
+    /// This mode will run when the key of the orbwalker is pressed
+    /// </summary>
+    internal class LastHit
+    {
+        /// <summary>
+        /// Put in here what you want to do when the mode is running
+        /// </summary>
+        public static void Execute()
+        {
+            Q.TryToCast(Q.GetLastHitMinion(), LasthitMenu);
+            W.TryToCast(W.GetLastHitMinion(), LasthitMenu);
+            E.TryToCast(E.GetLastHitMinion(), LasthitMenu);
+            R.TryToCast(R.GetLastHitMinion(), LasthitMenu);
+        }
+    }
+}
