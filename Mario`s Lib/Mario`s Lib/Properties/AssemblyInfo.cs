59e61d84254181df50913c023f993b7f7a5a60d3
Mario Gabriell <mariogk01@gmail.com>
2016-04-13 13:52:21 -0300

    dsasdas

--- Mario`s Lib/Mario`s Lib/Properties/AssemblyInfo.cs
+++ Mario`s Lib/Mario`s Lib/Properties/AssemblyInfo.cs
@@ -11,5 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3059f48c-5c9c-4107-8fdb-f7722678f0b5")]
-[assembly: AssemblyVersion("0.0.0.6")]
-[assembly: AssemblyFileVersion("0.0.0.6")]
+
+[assembly: AssemblyVersion("0.0.0.7")]
+[assembly: AssemblyFileVersion("0.0.0.7")]
