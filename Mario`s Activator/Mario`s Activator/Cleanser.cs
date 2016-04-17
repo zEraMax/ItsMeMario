ab50e3d778f9193c14bcecd1f1a4179a4825ca09
Mario Gabriell <mariogk01@gmail.com>
2016-04-17 15:00:55 -0300

    Fixed and improved the Cleanser

--- Mario`s Activator/Mario`s Activator/Cleanser.cs
+++ Mario`s Activator/Mario`s Activator/Cleanser.cs
@@ -24,31 +24,33 @@ namespace Mario_s_Activator
         public static void CleanseOnTick()
         {
             var delay = CleansersMenu.GetSliderValue("delayCleanse");
-
-            if (PlayerHasCleanse && Player.Instance.HasCC() && CleansersMenu.GetCheckBoxValue("check" + "cleanse"))
-            {
-                Core.DelayAction(() => Cleanse.Cast(), delay);
-            }
-
+            //Item
             var itemCleanse =
                 Cleansers.CleansersItems.FirstOrDefault(
-                    i => i.IsReady() && i.IsOwned() && CleansersMenu.GetCheckBoxValue("check" + (int)i.Id));
+                    i => i.IsReady() && i.IsOwned() && CleansersMenu.GetCheckBoxValue("check" + (int) i.Id));
 
-            if (itemCleanse != null)
+            if (itemCleanse == null) return;
+
+            if (itemCleanse.Id == ItemId.Mikaels_Crucible)
             {
-                if (itemCleanse.Id == ItemId.Mikaels_Crucible)
-                {
-                    var ally = EntityManager.Heroes.Allies.FirstOrDefault(a => a.HasCC());
-                    if (ally != null)
-                    {
-                        Core.DelayAction(() => itemCleanse.Cast(ally), delay);
-                    }
-                }
+                var ally = EntityManager.Heroes.Allies.FirstOrDefault(a => a.HasCC());
+                if (ally == null) return;
 
-                if (Player.Instance.HasCC())
-                {
-                    Core.DelayAction(() => itemCleanse.Cast(), delay);
-                }
+                Core.DelayAction(() => itemCleanse.Cast(ally), delay);
+                return;
+            }
+            if (Player.Instance.HasCC())
+            {
+                Core.DelayAction(() => itemCleanse.Cast(), delay);
+                return;
+            }
+
+            //SummonerSpell
+            if (!PlayerHasCleanse) return;
+            
+            if (Player.Instance.HasCC() && CleansersMenu.GetCheckBoxValue("check" + "cleanse"))
+            {
+                Core.DelayAction(() => Cleanse.Cast(), delay);
             }
         }
     }
